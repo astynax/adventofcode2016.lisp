fe28b03e76fc25a132dbfb8da7b154adb324e984
Aleksei Pirogov <astynax@users.noreply.github.com>
2023-02-04 20:14:40 +0400

    do some refactoring of day04

--- day04.lisp
+++ day04.lisp
@@ -19,21 +19,17 @@
     (loop for chunk in CHUNKS
           do (loop for c across chunk
                    do (incf (gethash c m 0))))
-    (let ((freqs
-            (sort (loop for k being each hash-key in m
-                        collect (cons (gethash k m) k))
-                  (lambda (pair1 pair2)
-                    (destructuring-bind
-                        ((v1 . k1) (v2 . k2))
-                        (list pair1 pair2)
-                      (or (> v1 v2)
-                          (and (= v1 v2)
-                               (char< k1 k2))))))))
-      (loop with cs = (make-string 5)
-            for i from 0 below 5
-            for (_ . v) in freqs
-            do (setf (char cs i) v)
-            finally (return cs)))))
+    (map 'string
+         #'cdr
+         (sort (loop for k being each hash-key in m
+                     collect (cons (gethash k m) k))
+               (lambda (pair1 pair2)
+                 (destructuring-bind
+                     ((v1 . k1) (v2 . k2))
+                     (list pair1 pair2)
+                   (or (> v1 v2)
+                       (and (= v1 v2)
+                            (char< k1 k2)))))))))
 
 (defun valid-p (LINE)
   (string= (second LINE)
@@ -55,11 +51,7 @@
   (loop with id = (first LINE)
         with chunks = (third LINE)
         for chunk in chunks
-        collect (loop with out = (make-string (length chunk))
-                      for i from 0
-                      for c across chunk
-                      do (setf (char out i) (rot id c))
-                      finally (return out))))
+        collect (map 'string (lambda (c) (rot id c)) chunk)))
 
 (defun solution2 (LINES)
   (loop for line in LINES
