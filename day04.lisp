a59b5c24d704b610405501373feb24f265134ac5
Aleksei Pirogov <astynax@users.noreply.github.com>
2023-02-07 17:29:49 +0400

    rewrite day04.lisp for the record

--- day04.lisp
+++ day04.lisp
@@ -2,64 +2,59 @@
   (let* ((chunks (uiop:split-string
                   LINE :separator '(#\-)))
          (last-chunk (car (last chunks))))
-    (multiple-value-bind (id id-stop)
-        (parse-integer last-chunk
-                       :junk-allowed t)
-      (let ((cs (subseq last-chunk
-                        (1+ id-stop)
-                        (1- (length last-chunk)))))
-        (list id cs (butlast chunks))))))
+    (destructuring-bind (id cs)
+        (uiop:split-string last-chunk :separator '(#\[))
+      (list (parse-integer id)
+            (subseq cs 0 (1- (length cs)))
+            (butlast chunks)))))
 
 (defun input ()
   (loop for line in (uiop:read-file-lines "day04.input")
         collect (decode-line line)))
 
-(defun get-cs (CHUNKS)
+(defun comparator (PAIR1 PAIR2)
+  (destructuring-bind (v1 . k1) PAIR1
+    (destructuring-bind (v2 . k2) PAIR2
+      (or (> v1 v2)
+          (and (= v1 v2)
+               (char< k1 k2))))))
+
+(defun checksum (CHUNKS)
   (let ((m (make-hash-table)))
     (loop for chunk in CHUNKS
           do (loop for c across chunk
                    do (incf (gethash c m 0))))
-    (map 'string
-         #'cdr
-         (sort (loop for k being each hash-key in m
-                     collect (cons (gethash k m) k))
-               (lambda (pair1 pair2)
-                 (destructuring-bind
-                     ((v1 . k1) (v2 . k2))
-                     (list pair1 pair2)
-                   (or (> v1 v2)
-                       (and (= v1 v2)
-                            (char< k1 k2)))))))))
+    (let ((pairs (loop for k being each hash-key of m
+                       collect (cons (gethash k m) k))))
+      (map 'string #'cdr
+           (subseq (sort pairs #'comparator) 0 5)))))
 
 (defun valid-p (LINE)
   (string= (second LINE)
-           (get-cs (third LINE))))
+           (checksum (third LINE))))
 
 (defun solution1 (LINES)
   (loop for line in LINES
-        for id = (first line)
         when (valid-p line)
-          summing id))
+          summing (first line)))
 
 ;; (solution1 (input))
 ;; => 185371
 
-(defun rot (N CHR)
-  (code-char (+ 97 (mod (+ (- (char-code CHR) 97) N) 26))))
+(defun make-decoder (SHIFT)
+  (lambda (CHR)
+    (code-char (+ 97 (mod (+ (- (char-code CHR) 97) SHIFT) 26)))))
 
-(defun decypher-line (LINE)
-  (loop with id = (first LINE)
-        with chunks = (third LINE)
-        for chunk in chunks
-        collect (map 'string (lambda (c) (rot id c)) chunk)))
+(defun decypher (LINE)
+  (loop with decoder = (make-decoder (first LINE))
+        for word in (third LINE)
+        collect (map 'string decoder word)))
 
 (defun solution2 (LINES)
   (loop for line in LINES
-        when (and (valid-p line)
-                  (member "northpole"
-                          (decypher-line line)
-                          :test #'equal))
+        for message = (decypher line)
+        when (member "northpole" message :test #'equal)
           do (return (first line))))
 
 ;; (solution2 (input))
-;; 984
+;; => 984
