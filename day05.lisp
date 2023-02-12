8723070397462fa5c627df3eadff66d49f78121d
Aleksei Pirogov <astynax@users.noreply.github.com>
2023-02-12 09:33:13 +0400

    rewrite day 05 for the record

--- day05.lisp
+++ day05.lisp
@@ -1,18 +1,17 @@
 (ql:quickload :md5)
 
-(defparameter input "abbhdwsy")
+(defparameter input
+  (string-trim '(#\NEWLINE)
+               (uiop:read-file-string "day05.input")))
 
-(defun is-good (MD5)
-  (and (= 0 (first MD5))
-       (= 0 (second MD5))
-       (> 16 (third MD5))))
-
-(defun ->char (MD5)
-  (format nil "~X" (third MD5)))
-
-(defun md5-bytes (PREFIX INDEX)
+(defun md5-list (PREFIX INDEX)
   (coerce (md5:md5sum-string (format nil "~a~a" PREFIX INDEX))
           'list))
+
+(defun good-hash-p (SUM)
+  (and (zerop (first SUM))
+       (zerop (second SUM))
+       (< (third SUM) 16)))
 
 (defun solution1 (PREFIX)
   (format
@@ -20,8 +19,8 @@
    (loop with counter = 0
          while (< counter 8)
          for i from 0
-         for h = (md5-bytes PREFIX i)
-         when (is-good h)
+         for h = (md5-list PREFIX i)
+         when (good-hash-p h)
            do (incf counter)
            and collect (third h))))
 
@@ -29,22 +28,22 @@
 ;; => "801B56A7"
 
 (defun solution2 (PREFIX)
-  (loop with o = (make-hash-table)
-        while (< (hash-table-count o) 8)
-        for i from 0
-        for h = (md5-bytes PREFIX i)
-        for b = (third h)
-        when (and (is-good h)
-                  (< b 8)
-                  (not (gethash b o)))
-          do (setf (gethash b o)
-                   (floor (/ (fourth h) 16)))
-        finally
-           (return
-             (format
-              nil "~{~X~}"
-              (loop for i from 0 to 7
-                    collect (gethash i o))))))
+  (format
+   nil "~{~X~}"
+   (loop with o = (make-hash-table)
+         while (< (hash-table-count o) 8)
+         for i from 0
+         for h = (md5-list PREFIX i)
+         for k = (third h)
+         when (and (good-hash-p h)
+                   (< k 8)
+                   (not (gethash k o)))
+           do (setf (gethash k o)
+                    (floor (/ (fourth h) 16)))
+         finally
+            (return
+              (loop for k from 0 to 7
+                    collect (gethash k o))))))
 
-(solution2 input)
+;; (solution2 input)
 ;; => "424A0197"
